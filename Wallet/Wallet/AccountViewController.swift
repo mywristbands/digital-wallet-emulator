572f311b46aadf5e002ff7bf2f5d34000a976a9d
Elias Heffan <41599300+mywristbands@users.noreply.github.com>
2019-11-04 00:21:08 -0800

    Transferring money works

--- Wallet/Wallet/AccountViewController.swift
+++ Wallet/Wallet/AccountViewController.swift
@@ -12,9 +12,15 @@ protocol UpdateHomeViewControllerDelegate : NSObjectProtocol{
     func accountsUpdated()
 }
 
-class AccountViewController: UIViewController {
+class AccountViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
     @IBOutlet weak var accountLabel: UILabel!
     @IBOutlet weak var amountLabel: UILabel!
+    
+    @IBOutlet weak var transferPopup: UIView!
+    @IBOutlet weak var accountPicker: UIPickerView!
+    @IBOutlet weak var transferAmountField: UITextField!
+    @IBOutlet weak var transferDoneButton: UIButton!
     
     @IBOutlet weak var depositButton: UIButton!
     @IBOutlet weak var withdrawButton: UIButton!
@@ -29,16 +35,50 @@ class AccountViewController: UIViewController {
     var ID = ""
     var amount: Double = 0
     var buttons: Array<UIButton> = []
+    var pickerOptions: Array<Account> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        buttons = [depositButton, withdrawButton, transferButton, deleteButton]
         
+        accountLabel.text = "\(name)"
+        amountLabel.text = "$" + String(format: "%0.02f", amount)
+        
+        buttons = [depositButton, withdrawButton, transferButton, deleteButton]
         for button in buttons {
             button.layer.cornerRadius = button.frame.height / 2
         }
-        accountLabel.text = "\(name)"
-        amountLabel.text = "$" + String(format: "%0.02f", amount)
+        
+        setupPopup()
+    }
+    
+    func setupPopup() {
+        func setupPopupAppearance() {
+            transferPopup.layer.shadowColor = UIColor.darkGray.cgColor
+            transferPopup.layer.shadowOpacity = 0.7
+            transferPopup.layer.shadowOffset = CGSize(width: 3, height: 3)
+            transferPopup.layer.shadowRadius = 15.0
+            transferPopup.layer.masksToBounds = false
+            
+            transferPopup.isHidden = true
+            transferPopup.layer.cornerRadius = transferPopup.frame.height / 10
+        }
+        func setupPopupInternals() {
+            accountPicker.delegate = self
+            accountPicker.dataSource = self
+
+            for account in walletContainingAccount.accounts {
+                if account.ID != self.ID {
+                    pickerOptions.append(account)
+                }
+            }
+            
+            transferAmountField.placeholder = "Amount"
+            transferAmountField.keyboardType = UIKeyboardType.decimalPad
+            
+            transferDoneButton.layer.cornerRadius = transferDoneButton.frame.height / 2
+        }
+        setupPopupAppearance()
+        setupPopupInternals()
     }
     
     @IBAction func onDone() {
@@ -99,6 +139,36 @@ class AccountViewController: UIViewController {
         self.present(alert, animated: true)
     }
     
+    @IBAction func onTransfer() {
+        transferPopup.isHidden = false
+        transferAmountField.becomeFirstResponder()
+    }
+    
+    @IBAction func onTransferDone() {
+        guard let transferAmountString = transferAmountField.text else {return}
+        guard var transferAmountDouble = Double(transferAmountString) else {return}
+        
+        // Don't let user transfer more money than they have in their account
+        if transferAmountDouble > self.amount {
+            transferAmountDouble = self.amount
+        }
+        
+        let selectedAccount = pickerOptions[accountPicker.selectedRow(inComponent: 0)]
+        guard let selectedAccountIndexInWallet = walletContainingAccount.accounts.firstIndex (where: { $0.ID == selectedAccount.ID }) else {return}
+        
+        transferAmountField.resignFirstResponder()
+        UIApplication.shared.beginIgnoringInteractionEvents()
+        Api.transfer(wallet: self.walletContainingAccount, fromAccountAt: self.indexInWallet, toAccountAt: selectedAccountIndexInWallet, amount: transferAmountDouble, completion: { (response, error) in
+                if error == nil {
+                    self.homeViewDelegate?.accountsUpdated()
+                    self.amount -= transferAmountDouble
+                    self.amountLabel.text = "$" + String(format: "%0.02f", self.amount)
+                    self.transferPopup.isHidden = true
+                    UIApplication.shared.endIgnoringInteractionEvents()
+                }
+        })
+    }
+    
     @IBAction func onDelete() {
         Api.removeAccount(wallet: walletContainingAccount, removeAccountat: indexInWallet, completion: { (_ response: [String: Any]?, _ error: Api.ApiError?) -> Void in
             self.homeViewDelegate?.accountsUpdated()
@@ -107,29 +177,15 @@ class AccountViewController: UIViewController {
     }
     
     // MARK: PickerView Delegate and DataSource implementations
-//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-//        return 1
-//    }
-//
-//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-//        return choices.count
-//    }
-//
-//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-//        return choices[row]
-//    }
-//
-//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-//        if row == 0 {
-//            typeValue = "Toyota"
-//        } else if row == 1 {
-//            typeValue = "Honda"
-//        } else if row == 2 {
-//            typeValue = "Chevy"
-//        } else if row == 3 {
-//            typeValue = "Audi"
-//        } else if row == 4 {
-//            typeValue = "BMW"
-//        }
-//    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerOptions.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "\(pickerOptions[row].name) $\(String(format: "%0.02f", pickerOptions[row].amount))"
+    }
 }
